6679c891c9ac32b5910a814a0dfca34208f99b68
ninggaocong <ninggaocong@wondersgroup.com>
2019-09-12 17:46:05 +0800

    spring cloud 微服务构建中、注册过程未完成

--- spring-microservice-cloud-demo/micro-user-service/src/main/java/com/ninggc/microuserservice/MicroUserServiceApplication.java
+++ spring-microservice-cloud-demo/micro-user-service/src/main/java/com/ninggc/microuserservice/MicroUserServiceApplication.java
@@ -3,17 +3,35 @@ package com.ninggc.microuserservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
-@PropertySource("classpath:morphia/morphia.properties")
+@EnableEurekaClient
+//@PropertySource("classpath:morphia/morphia.properties")
 public class MicroUserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroUserServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerPersonApi10() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ninggc.mocrouserservice.web"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
     }
 }
