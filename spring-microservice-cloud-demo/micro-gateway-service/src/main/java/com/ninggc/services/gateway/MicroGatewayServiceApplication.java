c0c818a2125421e0ea2b46c8cdd97d57aa6858fd
ninggaocong <ninggaocong@wondersgroup.com>
2019-09-17 10:24:30 +0800

    rename gateway-application to micro-gateway-service-application

--- spring-microservice-cloud-demo/micro-gateway-service/src/main/java/com/ninggc/services/gateway/MicroGatewayServiceApplication.java
+++ spring-microservice-cloud-demo/micro-gateway-service/src/main/java/com/ninggc/services/gateway/MicroGatewayServiceApplication.java
@@ -6,11 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayApplication {
-
+public class MicroGatewayServiceApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(MicroGatewayServiceApplication.class, args);
 	}
 	
 }
