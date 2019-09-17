80b5d62ba4b44a639c41c922a67fa805080f9f39
ninggaocong <ninggaocong@wondersgroup.com>
2019-09-17 10:18:50 +0800

    add gateway-service as you see

--- spring-microservice-cloud-demo/micro-manage-service/src/main/java/com/ninggc/micromanageservice/controller/ManageController.java
+++ spring-microservice-cloud-demo/micro-manage-service/src/main/java/com/ninggc/micromanageservice/controller/ManageController.java
@@ -3,7 +3,6 @@ package com.ninggc.micromanageservice.controller;
 import com.ninggc.micromanageservice.client.UserClient;
 import com.ninggc.microuserservice.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
