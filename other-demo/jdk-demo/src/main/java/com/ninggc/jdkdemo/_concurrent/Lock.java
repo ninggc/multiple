b9074fc37f67c4442684cbb8df6ec03dd336bd7c
ninggaocong <ninggaocong@wondersgroup.com>
2020-09-08 17:20:34 +0800

    jdk aqs

--- other-demo/jdk-demo/src/main/java/com/ninggc/jdkdemo/_concurrent/Lock.java
+++ other-demo/jdk-demo/src/main/java/com/ninggc/jdkdemo/_concurrent/Lock.java
@@ -1,4 +1,38 @@
 package com.ninggc.jdkdemo._concurrent;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Lock {
+    static ExecutorService executorService = Executors.newCachedThreadPool();
+    static ReentrantLock reentrantLock = new ReentrantLock();
+    static Integer a = 0;
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    // synchronized (a) {
+                    //     System.out.print(++a + "\t");
+                    // }
+
+                    try {
+                        Thread.sleep(2 * 1000);
+                        Thread.yield();
+                        reentrantLock.lock();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (reentrantLock.isLocked()) {
+                            reentrantLock.unlock();
+                        }
+                    }
+
+                }
+            });
+        }
+    }
 }
