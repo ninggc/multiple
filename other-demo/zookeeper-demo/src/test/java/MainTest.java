cfa476212be1cfc66563650cb25e6db19ca98176
ninggc <906978985@qq.com>
2020-11-24 21:59:11 +0800

    io

---
+++ other-demo/zookeeper-demo/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.I0Itec.zkclient.ZkClient;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void test1() {
+        ZkClient zkClient = new ZkClient("wonders.ninggc.cn:2181");
+
+        System.out.println("");
+    }
+}
