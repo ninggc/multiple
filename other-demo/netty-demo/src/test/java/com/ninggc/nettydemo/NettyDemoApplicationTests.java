9335058feec79b22f67e492aa7ee557fbd613d9f
ninggc <ninggc@foxmail.com>
2021-06-02 23:41:47 +0800

    feat(netty-demo): start to learn netty

--- other-demo/netty-demo/src/test/java/com/ninggc/nettydemo/NettyDemoApplicationTests.java
+++ other-demo/netty-demo/src/test/java/com/ninggc/nettydemo/NettyDemoApplicationTests.java
@@ -1,12 +1,10 @@
 package com.ninggc.nettydemo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NettyDemoApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
