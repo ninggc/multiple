e00ae5e8a0a97909937a3054f072f1e2215f74f1
ninggc <ninggc@foxmail.com>
2021-08-22 00:12:12 +0800

    feat: mongo-demo

--- other-demo/mongo-demo/src/main/java/com/ninggc/mongodemo/service/TestServiceImpl.java
+++ other-demo/mongo-demo/src/main/java/com/ninggc/mongodemo/service/TestServiceImpl.java
@@ -2,7 +2,6 @@ package com.ninggc.mongodemo.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
@@ -18,31 +17,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class TestServiceImpl implements TestService {
 
-    @Resource
-    private MongoTemplate mongoTemplate;
-    @Value("${mongo.collectionName}")
+    private final MongoTemplate mongoTemplate;
+    @Value("${mongo.collectionName:test1}")
     private String collectionName;
-    // @Resource
-    // private TransactionTemplate transactionTemplate;
+
+    public TestServiceImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
 
     @Override
     @Transactional
     public void test() {
-        // transactionTemplate.execute(new TransactionCallback<Object>() {
-        //     @Override
-        //     public Object doInTransaction(TransactionStatus transactionStatus) {
-        //         mongoTemplate.insert(new HashMap<>());
-        //         return null;
-        //     }
-        // });
-
         Map<Object, Object> objectToSave = new HashMap<>();
         objectToSave.put("a", 1);
         mongoTemplate.insert(objectToSave, collectionName);
-
-        HashMap<Object, Object> map2 = new HashMap<>();
-        map2.put("map2", 1);
-        mongoTemplate.insert(map2, collectionName);
 
         if (false) {
             throw new RuntimeException("e");
---
+++ other-demo/mongo-demo/src/test/java/com/ninggc/mongodemo/driver/MongoConfigTest.java
@@ -0,0 +1,33 @@
+package com.ninggc.mongodemo.driver;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class MongoConfigTest {
+
+    static MongoClient mongoClient;
+
+    @BeforeAll
+    static void beforeAll() {
+        // Replace the uri string with your MongoDB deployment's connection string.
+        // String connectionString = "mongodb+srv://<username>:<password>@<cluster-address>/test?w=majority";
+        String connectionString = "mongodb://191.168.1.201:27017/ninggc?w=majority";
+        ConnectionString connString = new ConnectionString(connectionString);
+        MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connString)
+            .retryWrites(true)
+            .build();
+        mongoClient = MongoClients.create(settings);
+    }
+
+    @Test
+    void listCollections() {
+
+        System.out.println(String.join(", ", mongoClient.listDatabaseNames()));
+
+    }
+}
