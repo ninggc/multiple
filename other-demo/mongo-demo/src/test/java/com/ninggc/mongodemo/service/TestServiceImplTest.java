031939c857abb8af84227c75c34b99ec020df987
ninggc <ninggc@foxmail.com>
2021-10-22 00:43:51 +0800

    feat: update mongo-demo

--- other-demo/mongo-demo/src/test/java/com/ninggc/mongodemo/service/TestServiceImplTest.java
+++ other-demo/mongo-demo/src/test/java/com/ninggc/mongodemo/service/TestServiceImplTest.java
@@ -23,11 +23,12 @@ class TestServiceImplTest {
     @Resource
     private MongoTemplate mongoTemplate;
 
+    final String collectionName = "test";
+
     @Test
     void test1() {
         // testService.test();
         MongoValuePO po = new MongoValuePO(null, new HashMap<>(), null);
-        String collectionName = "test";
         MongoValuePO insert = mongoTemplate.insert(po, collectionName);
 
         String id = insert.getId();
@@ -45,5 +46,25 @@ class TestServiceImplTest {
         Map map = mongoTemplate.findById(id, Map.class, collectionName);
         List<MongoValuePO> all = mongoTemplate.findAll(MongoValuePO.class, collectionName);
 
+    }
+
+    @Test
+    void testFind() {
+        String id = "61718c392ba5f64b235a2201";
+        ObjectId objectId = new ObjectId(id);
+        List<Map> map1 = mongoTemplate.find(Query.query(Criteria.where("id").is(id)), Map.class, collectionName);
+        List<Map> map2 = mongoTemplate.find(Query.query(Criteria.where("id").is(objectId)), Map.class, collectionName);
+        List<Map> map3 = mongoTemplate.find(Query.query(Criteria.where("_id").is(id)), Map.class, collectionName);
+        List<Map> map4 = mongoTemplate.find(Query.query(Criteria.where("_id").is(objectId)), Map.class, collectionName);
+
+        List<MongoValuePO> map5 = mongoTemplate.find(Query.query(Criteria.where("id").is(id)), MongoValuePO.class, collectionName);
+        List<MongoValuePO> map6 = mongoTemplate.find(Query.query(Criteria.where("id").is(objectId)), MongoValuePO.class, collectionName);
+        List<MongoValuePO> map7 = mongoTemplate.find(Query.query(Criteria.where("_id").is(id)), MongoValuePO.class, collectionName);
+        List<MongoValuePO> map8 = mongoTemplate.find(Query.query(Criteria.where("_id").is(objectId)), MongoValuePO.class, collectionName);
+
+        Map m1 = mongoTemplate.findById(id, Map.class, collectionName);
+        Map m2 = mongoTemplate.findById(objectId, Map.class, collectionName);
+        MongoValuePO m3 = mongoTemplate.findById(id, MongoValuePO.class, collectionName);
+        MongoValuePO m4 = mongoTemplate.findById(objectId, MongoValuePO.class, collectionName);
     }
 }
