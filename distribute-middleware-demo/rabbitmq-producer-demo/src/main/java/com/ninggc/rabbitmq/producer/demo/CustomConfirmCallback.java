a366099d27b2779d5f8f947bf24145c19fcec56a
ninggc <ninggc@foxmail.com>
2021-12-01 00:10:03 +0800

    feat: fix pom.xml

--- distribute-middleware-demo/rabbitmq-producer-demo/src/main/java/com/ninggc/rabbitmq/producer/demo/CustomConfirmCallback.java
+++ distribute-middleware-demo/rabbitmq-producer-demo/src/main/java/com/ninggc/rabbitmq/producer/demo/CustomConfirmCallback.java
@@ -1,4 +1,4 @@
-package com.ninggc.demo.rabbitmq.producer;
+package com.ninggc.rabbitmq.producer.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
